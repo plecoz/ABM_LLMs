a20a2db847de7a46c175bd2d15dc91f4dd9230ab
plecoz <pierrelecoz35@gmail.com>
2025-05-13 12:10:35 +0800

    update visualization (not sure...)

--- visualization/agent_plot.py
+++ visualization/agent_plot.py
@@ -3,6 +3,7 @@ class AgentPlotter:
         self.base = base_map
         self.resident_markers = []
         self.poi_markers = []
+        self.organizations_markers = []
     
     def plot_pois(self, pois, graph):
         """Plot POIs as red squares."""
@@ -17,3 +18,9 @@ class AgentPlotter:
             x, y = graph.nodes[resident.current_node]['x'], graph.nodes[resident.current_node]['y']
             m = self.base.ax.plot(x, y, 'o', color='deepskyblue', markersize=6)[0]
             self.resident_markers.append(m)
+    def plot_organizations(self, organizations, graph):
+        """Plot residents as blue circles."""
+        for organization in organizations:
+            x, y = graph.nodes[organization.current_node]['x'], graph.nodes[organization.current_node]['y']
+            m = self.base.ax.plot(x, y, 'o', color='green', markersize=8)[0]
+            self.organizations_markers.append(m)
